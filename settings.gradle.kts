fc40f6f3a43fd4258781c2b728ea9b274140627b
MATIAS DEL VECCHIO <matias.delv.dom@gmail.com>
2024-04-18 19:14:14 -0300

    Modules creation:   - Data   - Domain   - FeatureHome   - FeatureLogin   - FeatureProductDetails   - UI

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,4 +15,9 @@ dependencyResolutionManagement {
 
 rootProject.name = "MobiStock"
 include(":app")
- 
+include(":ui")
+include(":domain")
+include(":data")
+include(":featureHome")
+include(":featureLogin")
+include(":featureProductDetails")
