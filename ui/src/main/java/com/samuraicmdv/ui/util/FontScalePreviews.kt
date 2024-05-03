acecf3706bd3f73496dec9e93d39b6678f4ef3fc
dvmatias <matias.delv.dom@gmail.com>
2024-05-03 16:31:20 -0300

    FEATURE HOME:   - Added global preview annotations for use within the entire app: ThemePreviews, FontScalePreviews and OrientationPreviews

---
+++ ui/src/main/java/com/samuraicmdv/ui/util/FontScalePreviews.kt
@@ -0,0 +1,7 @@
+package com.samuraicmdv.ui.util
+
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(name = "Default Font Size", fontScale = 1f)
+@Preview(name = "Large Font Size", fontScale = 1.5f)
+annotation class FontScalePreviews
