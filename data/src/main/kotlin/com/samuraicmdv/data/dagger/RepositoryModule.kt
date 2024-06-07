4881e630c77d7f706661bb19680b31aceb600ce1
MATIAS DEL VECCHIO <matias.delv.dom@gmail.com>
2024-06-07 16:00:12 -0300

    - Added Products repository + implementation

--- data/src/main/kotlin/com/samuraicmdv/data/dagger/RepositoryModule.kt
+++ data/src/main/kotlin/com/samuraicmdv/data/dagger/RepositoryModule.kt
@@ -2,16 +2,19 @@ package com.samuraicmdv.data.dagger
 
 import com.samuraicmdv.data.datasource.BrandDataSource
 import com.samuraicmdv.data.datasource.CategoryDataSource
-import com.samuraicmdv.data.datasource.retrofit.UserDataSourceRetrofitImpl
+import com.samuraicmdv.data.datasource.ProductDataSource
 import com.samuraicmdv.data.datasource.retrofit.LoginDataSourceRetrofitImpl
+import com.samuraicmdv.data.datasource.retrofit.UserDataSourceRetrofitImpl
 import com.samuraicmdv.data.repository.BrandRepositoryImpl
 import com.samuraicmdv.data.repository.CategoryRepositoryImpl
-import com.samuraicmdv.data.repository.UserRepositoryImpl
 import com.samuraicmdv.data.repository.LoginRepositoryImpl
+import com.samuraicmdv.data.repository.ProductRepositoryImpl
+import com.samuraicmdv.data.repository.UserRepositoryImpl
 import com.samuraicmdv.domain.repository.BrandRepository
 import com.samuraicmdv.domain.repository.CategoryRepository
-import com.samuraicmdv.domain.repository.UserRepository
 import com.samuraicmdv.domain.repository.LoginRepository
+import com.samuraicmdv.domain.repository.ProductRepository
+import com.samuraicmdv.domain.repository.UserRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -35,5 +38,9 @@ object RepositoryModule {
     @Provides
     fun provideBrandRepository(brandDataSource: BrandDataSource): BrandRepository =
         BrandRepositoryImpl(brandDataSource)
+
+    @Provides
+    fun provideProductRepository(productDataSource: ProductDataSource): ProductRepository =
+        ProductRepositoryImpl(productDataSource)
 
 }
---
+++ data/src/main/kotlin/com/samuraicmdv/data/repository/ProductRepositoryImpl.kt
@@ -0,0 +1,42 @@
+package com.samuraicmdv.data.repository
+
+import com.samuraicmdv.data.datasource.ProductDataSource
+import com.samuraicmdv.domain.model.CreateProductResponseModel
+import com.samuraicmdv.domain.repository.ProductRepository
+import com.samuraicmdv.domain.util.ResponseWrapper
+import javax.inject.Inject
+
+class ProductRepositoryImpl @Inject constructor(
+    private val productDataSource: ProductDataSource
+) : ProductRepository {
+    override suspend fun createProduct(
+        name: String,
+        shortDescription: String,
+        longDescription: String,
+        code: String?,
+        model: String?,
+        categoryId: Int,
+        brandId: Int,
+        sku: String?,
+        selling: String?,
+        cost: String?,
+        currencyId: String?,
+        storeId: String?,
+        preferredMargin: Int?
+    ): ResponseWrapper<CreateProductResponseModel> =
+        productDataSource.createProduct(
+            name = name,
+            shortDescription = shortDescription,
+            longDescription = longDescription,
+            code = code,
+            model = model,
+            categoryId = categoryId,
+            brandId = brandId,
+            sku = sku,
+            selling = selling,
+            cost = cost,
+            currencyId = currencyId,
+            storeId = storeId,
+            preferredMargin = preferredMargin
+        )
+}
